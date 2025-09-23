9d1e7a8a79f4e25d0e84095b3d8df060069e6816
ART0065570 <ART0065570@SEN464937.educa.sf>
2025-09-23 20:15:50 -0300

    Init Commit

---
+++ src/main/java/com/oficinaos/oficinaos/OficinaosApplication.java
@@ -0,0 +1,13 @@
+package com.oficinaos.oficinaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinaosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oficinaos/oficinaos/OficinaosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oficinaos.oficinaos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
